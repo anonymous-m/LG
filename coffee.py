26f4ed245a574acc3fa399c8936cf2441bfbba8a
anonymous-m <mahmoudyassin98@gmail.com>
2024-01-14 19:57:53 -0500

    4th commit

--- coffee.py
+++ coffee.py
@@ -1,5 +1,6 @@
 def name():
-    return "Mohamed","yassin"
+    return "Mohamed","yassin","yassin1","yassin2"
+
 
 # print the tuple with the returned values
 print(name())
@@ -7,3 +8,4 @@ print(name())
 # get the individual items
 name_1, name_2 = name()
 print(name_1, name_2)
+print(name_4, name_3)
